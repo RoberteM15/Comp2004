f620b37d2fd90bab39853341e0a4846686c798ad
Roberttheo <146362994+Roberttheo@users.noreply.github.com>
2023-10-24 23:45:43 -0400

    Add files via upload

---
+++ Labs/Lab 4/src/ColorBox.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+export default function ColorBox({ colorArray }) {
+
+    const colorsCount = colorArray.length;
+    const [color, setColor] = useState(colorArray[Math.floor(Math.random() * colorsCount)])
+
+    return (
+        <div className="ColorBox" 
+            style={{backgroundColor: color }}
+            onClick={() => setColor(colorArray[Math.floor(Math.random() * colorsCount)])}
+        >
+        </div>
+    )
+}
