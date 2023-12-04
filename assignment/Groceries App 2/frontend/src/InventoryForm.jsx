b4c3c50b926cb1661cd3c5b70185cf9d9e665998
Roberttheo <146362994+Roberttheo@users.noreply.github.com>
2023-12-03 22:14:52 -0500

    Add files via upload

---
+++ assignment/Groceries App 2/frontend/src/InventoryForm.jsx
@@ -0,0 +1,88 @@
+import { useEffect } from "react"
+import {useForm} from "react-hook-form"
+
+//created a function Inventory form to allow an product to be add to the inventory
+export default function InventoryForm({
+    formData, 
+    handleOnChange, 
+    handleOnSubmit,
+    toggleEdit
+}) {
+    const {id} = formData
+    const {register, handleSubmit, formState: {errors}, reset} = useForm({
+        defaultValues: id ? formData : 
+        {
+            id: "Default",
+            productName: "Default",
+            brand: "Default",
+            quantity: "Default",
+            image: "Default",
+            price: "Default"
+    }})
+    
+    useEffect(() => reset(formData), [toggleEdit])
+
+    return (
+        <div>
+            {/* created a form to add new products to the database and inventory */}
+            <form action="" onSubmit={ handleSubmit(handleOnSubmit) }>
+                <div>
+                    <label htmlFor="productName">Product Name</label>
+                    <input
+                        type="text"
+                        {...register("productName", {required: "Please enter a valid product name. Thanks"})}
+                        id="productName"
+                        onChange={handleOnChange}
+                        value={formData.productName}
+                    />
+                    <span>{errors.productName?.message}</span>
+                </div>
+                <div>
+                    <label htmlFor="brand">Brand</label>
+                    <input 
+                        type="text"
+                        {...register("brand", {required: "Please enter a valid brand. Thanks"})}
+                        id="brand"
+                        onChange={handleOnChange}
+                        value={formData.brand}
+                    />
+                    <span>{errors.brand?.message}</span>
+                </div>
+                <div>
+                    <label htmlFor="quantity">Quantity</label>
+                    <input 
+                        type="text"
+                        {...register("quantity", {required: "Please enter a valid quantity. Thanks"})}
+                        id="quantity"
+                        onChange={handleOnChange}
+                        value={formData.quantity}
+                    />
+                    <span>{errors.quantity?.message}</span>
+                </div>
+                <div>
+                    <label htmlFor="image">Image URL</label>
+                    <input 
+                        type="text"
+                        {...register("image", {required: "Please enter a valid image url. Thanks"})}
+                        id="image"
+                        onChange={handleOnChange}
+                        value={formData.image}
+                    />
+                    <span>{errors.image?.message}</span>
+                </div>
+                <div>
+                    <label htmlFor="price">Price</label>
+                    <input 
+                        type="text"
+                        {...register("price", {required: "Please enter a valid price. Thanks"})}
+                        id="price"
+                        onChange={handleOnChange}
+                        value={formData.price}
+                    />
+                    <span>{errors.price?.message}</span>
+                </div>
+                <button>{toggleEdit ? `Edit ${formData.productName}` : "Add to Inventory"}</button>
+            </form>
+        </div>
+    )
+}
