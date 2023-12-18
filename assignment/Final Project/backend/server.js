d6b1ad0a3ba58f84ea36514cc339f4ca956482ea
RoberteM15 <146362994+RoberteM15@users.noreply.github.com>
2023-12-18 07:46:42 -0500

    Add files via upload

--- assignment/Final Project/backend/server.js
+++ assignment/Final Project/backend/server.js
@@ -4,16 +4,19 @@ const server = express()
 const mongoose = require("mongoose")
 const {request, response } = require("http")
 const User = require("./models/user.js")
+const Product = require("./models/product.js")
 const db_uri = "mongodb+srv://robertmatney:database1234@comp1013-cluster.yhuzwhc.mongodb.net/products?retryWrites=true&w=majority"
 const port = 3000
 const cors = require("cors")
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 
+//middleware
 server.use(express.urlencoded({ extended: false }))
 server.use(express.json())
 server.use(cors())
 
+//connect
 mongoose.connect(db_uri).then((result) => {
     server.listen(port, () => {console.log(`Listing on ${port}...\nConnect to DB`)
     })
@@ -27,6 +30,41 @@ server.get("/", (request, response) => {
 server.get("/products", async (request, response) => {
     const products = await Product.find()
     response.send(products)
+})
+
+server.post("/addProduct", async (request, response) => {
+    const product = request.body
+    const postProduct = new Product({
+        id: product.id,
+        productName: product.productName,
+        brand: product.brand,
+        quantity: product.quantity,
+        image: product.image,
+        price: product.price
+    })
+    const saveProduct = await postProduct.save()
+    saveProduct ? response.send("Product is added to the Inventory") 
+    : response.send("No new product was added. Please try again.")
+})
+
+server.delete("/product/:id", async (request, response) => {
+    const {id} = request.params
+    const deleteProduct = await Product.deleteOne({
+        _id: new mongoose.Types.ObjectId(id),
+    })
+    deleteProduct ? response.send(`${id} product has been deleted`) 
+    : response.send("The product was not deleted. Try again")
+})
+
+server.patch("/product/:id", async (request, response) => {
+    const {id} = request.params
+    const product = request.body
+    const patchProduct = await Product.updateOne(
+        { _id: new mongoose.Types.ObjectId(id)}, 
+        {$set: product }
+    )
+    patchProduct ? response.send(`${product.productName} has been edited`)
+    : response.send("Faild to edit the product")
 })
 
 server.post("/register", async (request, response) => {
